e6ec2eeb02edc6552a9769746ea60920dd890be5
bogusmogus <113585770+bogusmogus@users.noreply.github.com>
2023-06-01 17:42:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+echo "<a href='insert.php'>Voeg nieuwe klant toe</a></br>";
+echo "<a href='insertVerkoop.php'>Maak nieuwe verkooporder aan</a></br>";
+echo "<a href='selectVerkoop.php'>laat alle verkooporders zien</a></br>";
+?>
